0e8f1381e7eb1d36a85f1539618accf6944bb865
Brownerd <benjamin@browmerd.com>
2018-11-28 10:05:22 -0600

    tut stuff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Application from './Application';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Application from "./Application";
 
-import Amplify from 'aws-amplify';
-import configuration from './aws-exports';
+import Amplify from "aws-amplify";
+import configuration from "./aws-exports";
+
+import { Auth } from "aws-amplify";
 
 Amplify.configure(configuration);
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById("root"));
